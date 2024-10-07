849b478c129f20f11618202b027d09f599d152c4
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-10-07 21:32:03 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
